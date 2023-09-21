a6c6b8ec1e326c9991e08dd6808689f4a5f0359d
kenechukwu Anoliefo <keneanoliefo24@gmail.com>
2023-09-21 18:36:26 +0100

    practice

---
+++ practiceTime1/variable/contatenate-two-strings-in-a-function/Incrementing-and-decrementing/index.js
@@ -0,0 +1,19 @@
+let myPoint = 3
+
+// create two functions, add3Points() and removePoints(), and have them
+// call the functions to that line below logs out 10
+function add3Points() {
+    myPoints += 3
+}
+function remove1Point() {
+    myPoint -= 1
+
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
+
+console.log(myPoints)
