6522255f95db49c099d92d9454b6534f1cd1b4dd
kenechukwu Anoliefo <keneanoliefo24@gmail.com>
2023-10-08 19:43:53 +0100

    Finished all

---
+++ 7. Build a Chrome Extension/27. Template strings on multiple lines/index.js
@@ -0,0 +1,11 @@
+// template strings/literals
+
+const recipient = "Per"
+const sender = "Kenechukwu Anoliefo"
+
+// Break the email string into multiple lines
+const email = `
+Hey ${recipient}! 
+How is it going? Cheers ${sender}`
+
+console.log(email)
